b2da51e5212aa11b90caa643fe03b2604915c48c
big shep <shepapps@gmail.com>
2021-12-04 22:10:53 -0700

    black

--- src/PreSalePage/PreSale.js
+++ src/PreSalePage/PreSale.js
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./style.module.css";
 import { useEffect,useState } from "react";
+//import Background from '../imgs/goddess.jpeg';
 import Background from '../imgs/logo.jpg';
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -103,22 +104,27 @@ const PreSale = () => {
   };
 
   return (
-    <>
-      <div className={style.body} >
 
+      <body className="background"
+                                    style={{
 
-          <div className={style.body_wrapper} src={logo}>
-            <h1 style={{ textAlign: "center" }}>MINT YOUR OMNI</h1>
-            <h2 style={{ textAlign: "center" }}>HOW MANY OMNIS DO YOU WANT?</h2>
+                                      backgroundColor: 'black',
+                                      backgroundPosition: 'center',
+                                      backgroundSize: 'cover',
+                                      backgroundRepeat: 'no-repeat',
+                                      width: '100vw',
+                                      height: '100vh'
+                                    }} >
+
+            <h1 style={{ textAlign: "center", color:'white' }}>MINT YOUR OMNI</h1>
+            <h2 style={{ textAlign: "center", color:'white' }}>HOW MANY OMNIS DO YOU WANT?</h2>
      
             <DropdownButton
-
              style={{ textAlign: "center" }}
              title={numOmnis}
              id="dropdown-menu"
              onSelect={handleSelect}
-
-               >
+            >
                     <Dropdown.Item eventKey="1">1</Dropdown.Item>
                     <Dropdown.Item eventKey="2">2</Dropdown.Item>
                     <Dropdown.Item eventKey="3">3</Dropdown.Item>
@@ -129,31 +135,27 @@ const PreSale = () => {
                     <Dropdown.Item eventKey="8">8</Dropdown.Item>
                     <Dropdown.Item eventKey="9">9</Dropdown.Item>
                     <Dropdown.Item eventKey="10">10</Dropdown.Item>
-              </DropdownButton>
+            </DropdownButton>
 
+            <h2 style={{ textAlign: "center" }}></h2>
 
+            <div style={{ textAlign: "center" }}>
+              <a>
+                <img
+                  src={mint}
+                  alt="img"
+                  onClick={() => BuyToken(numOmnis)}
+                  width="300" 
+                  height="100"
+                  id="mint"
 
-              <div style={{ textAlign: "center" }}>
-                <a>
-                  <img
-                    src={mint}
-                    alt="img"
-                    onClick={() => BuyToken(numOmnis)}
-                    width="400" 
-                    height="100"
-                    id="mint"
-
-                  />
-                </a>
-
-
-
+                />
+              </a>
             </div>
 
-          <h2 style={{ textAlign: "center" }}>OMNIS ARE 0.05 ETH EACH</h2>
-          <h3 style={{ textAlign: "center" }}>(Reminder: every 2 mints gets you one FREE female NFT from our next collection)</h3>
-          <h3 style={{ textAlign: "center" }}>Breeding info coming soon</h3>
-
+          <h2 style={{ textAlign: "center",  color:'white'}}>OMNIS ARE 0.05 ETH EACH</h2>
+          <h3 style={{ textAlign: "center",  color:'white' }}>(Reminder: every 2 mints gets you one FREE female NFT from our next collection)</h3>
+          <h3 style={{ textAlign: "center",  color:'white' }}>Breeding info coming soon</h3>
 
 
           <p className={style.address} style={{ textAlign: "center" }}>
@@ -164,9 +166,10 @@ const PreSale = () => {
               <Button onClick={loadWeb3}>CONNECT WALLET</Button>
             </div>
           </div>
-        </div>
-      </div>
-    </>
+      </body>
+
+
+
   );
 };
 
---
+++ src/PreSalePage/htmlbackup.js
@@ -0,0 +1,60 @@
+      <body className="background"
+                                    style={{
+                                      backgroundColor: 'black',
+                                      backgroundPosition: 'center',
+                                      backgroundSize: 'cover',
+                                      backgroundRepeat: 'no-repeat',
+
+                                    }} >
+
+            <h1 style={{ textAlign: "center" }}>MINT YOUR OMNI</h1>
+            <h2 style={{ textAlign: "center" }}>HOW MANY OMNIS DO YOU WANT?</h2>
+     
+            <DropdownButton
+             style={{ textAlign: "center" }}
+             title={numOmnis}
+             id="dropdown-menu"
+             onSelect={handleSelect}
+            >
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">4</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">5</Dropdown.Item>
+                    <Dropdown.Item eventKey="6">6</Dropdown.Item>
+                    <Dropdown.Item eventKey="7">7</Dropdown.Item>
+                    <Dropdown.Item eventKey="8">8</Dropdown.Item>
+                    <Dropdown.Item eventKey="9">9</Dropdown.Item>
+                    <Dropdown.Item eventKey="10">10</Dropdown.Item>
+            </DropdownButton>
+
+            <h2 style={{ textAlign: "center" }}></h2>
+
+            <div style={{ textAlign: "center" }}>
+              <a>
+                <img
+                  src={mint}
+                  alt="img"
+                  onClick={() => BuyToken(numOmnis)}
+                  width="300" 
+                  height="100"
+                  id="mint"
+
+                />
+              </a>
+            </div>
+
+          <h2 style={{ textAlign: "center" }}>OMNIS ARE 0.05 ETH EACH</h2>
+          <h3 style={{ textAlign: "center" }}>(Reminder: every 2 mints gets you one FREE female NFT from our next collection)</h3>
+          <h3 style={{ textAlign: "center" }}>Breeding info coming soon</h3>
+
+
+          <p className={style.address} style={{ textAlign: "center" }}>
+            YOUR ADDRESS:{account}
+          </p>
+          <div style={{ textAlign: "center" }}>
+            <div className={style.wallet_btn}>
+              <Button onClick={loadWeb3}>CONNECT WALLET</Button>
+            </div>
+          </div>
+      </body>
