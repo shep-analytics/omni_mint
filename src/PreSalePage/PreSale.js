5cb2df3e3f1ecdf9894a7d3bb0494011b6350100
big shep <shepapps@gmail.com>
2021-12-05 22:41:08 -0700

    final

--- src/PreSalePage/PreSale.js
+++ src/PreSalePage/PreSale.js
@@ -3,8 +3,10 @@ import style from "./style.module.css";
 import { useEffect,useState } from "react";
 //import Background from '../imgs/goddess.jpeg';
 import Background from '../imgs/logo.jpg';
+//import Background from '../imgs/background.png';
 import header_picture from '../imgs/header_picture.png'
 
+import connect_wallet from '../imgs/connect_wallet.png'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -19,7 +21,7 @@ import { tokenABI } from "../contracts/ABIs";
 import { tokenAddress } from "../contracts/contractAddress";
 
 import mint from "../imgs/mint.png";
-import omni_text from "../imgs/writing.png"
+import omni_text from "../imgs/new_writing.png"
 import logo from "../imgs/logo.jpg"
 
 //import { ethers } from "ethers";
@@ -120,7 +122,77 @@ const PreSale = () => {
                                     }} >
 
 
-                                    <h1>presale over</h1>
+            <div style={{ textAlign: "center" }}>
+                <img
+                  src={header_picture}
+                  alt="img"
+                  width="1000" 
+                  height="200"
+                  id="mint"
+
+                />
+            </div>
+     
+            <DropdownButton
+             style={{ textAlign: "center" }}
+             title={numOmnis}
+             id="dropdown-menu"
+             onSelect={handleSelect}
+
+            >
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">4</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">5</Dropdown.Item>
+                    <Dropdown.Item eventKey="6">6</Dropdown.Item>
+                    <Dropdown.Item eventKey="7">7</Dropdown.Item>
+                    <Dropdown.Item eventKey="8">8</Dropdown.Item>
+                    <Dropdown.Item eventKey="9">9</Dropdown.Item>
+                    <Dropdown.Item eventKey="10">10</Dropdown.Item>
+                    <Dropdown.Item eventKey="10">15</Dropdown.Item>
+                    <Dropdown.Item eventKey="10">20</Dropdown.Item>
+            </DropdownButton>
+
+            <h2 style={{ textAlign: "center" }}></h2>
+
+            <div style={{ textAlign: "center" }}>
+              <a>
+                <img
+                  src={mint}
+                  alt="img"
+                  onClick={() => BuyToken(numOmnis)}
+                  width="300" 
+                  height="100"
+                  id="mint"
+
+                />
+              </a>
+            </div>
+
+
+            <div style={{ textAlign: "center" }}>
+                <img
+                  src={omni_text}
+                  alt="img"
+                  width="1000" 
+                  height="200"
+                  id="mint"
+
+                />
+
+            </div>
+
+
+
+          <p className={style.address} style={{ textAlign: "center" }}>
+            YOUR ADDRESS:{account}
+          </p>
+          <div style={{ textAlign: "center" }}>
+            <div className={style.wallet_btn}>
+              <Button onClick={loadWeb3}>CONNECT WALLET</Button>
+            </div>
+          </div>>
 
       </body>
 
